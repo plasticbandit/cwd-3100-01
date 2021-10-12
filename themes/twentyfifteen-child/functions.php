5e0e3c25b09888b8a2ef49afb2c52a9053a67b38
Nicola <nicola.mcfadden@dcmail.ca>
2021-10-12 12:27:21 -0400

    WordPress setup

---
+++ themes/twentyfifteen-child/functions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * *copied from parent template for reference
+ * Twenty Fifteen functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://developer.wordpress.org/themes/advanced-topics/child-themes/
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://developer.wordpress.org/plugins/}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+// define function first to eliminate potential order of operation issues
+function twentyfifteenchild_wp_enqueue_scripts() {
+    $parenthandle = 'twentyfifteen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style(
+        // input parameters:
+        $parenthandle,
+        get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        // heirarchy to access parent
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style(
+        'twentyfifteenchild-style',
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
+add_action( 'wp_enqueue_scripts', 'twentyfifteenchild_wp_enqueue_scripts' );
+
+// DO NOT END WITH PHP TAG - could cause rest of site to break
